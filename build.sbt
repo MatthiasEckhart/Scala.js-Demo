738cab9dc857654161bdefbbee5a05fc06eb8f5c
Matthias Eckhart <github@eckhart.me>
2016-11-05 21:36:11 +0100

    [TASK]: Init project.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Scala.js-Demo"
+
+version := "1.0"
+
+scalaVersion := "2.12.0"
+
+enablePlugins(ScalaJSPlugin)
---
+++ src/main/scala/me.eckhart.matthias/DemoApp.scala
@@ -0,0 +1,9 @@
+package me.eckhart.matthias
+
+import scala.scalajs.js.JSApp
+
+object DemoApp extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
