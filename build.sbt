d2744b1b44fe4338d582a551311edab7c786285d
Matthias Eckhart <github@eckhart.me>
2016-11-06 10:06:45 +0100

    [FEATURE]: Implemented a demonstration of a click listener.

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "1.0"
 scalaVersion := "2.12.0"
 
 enablePlugins(ScalaJSPlugin)
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
--- src/main/scala/me.eckhart.matthias/DemoApp.scala
+++ src/main/scala/me.eckhart.matthias/DemoApp.scala
@@ -1,9 +1,23 @@
 package me.eckhart.matthias
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
 
 object DemoApp extends JSApp {
   def main(): Unit = {
+    /* Console.log("Hello world!"); */
     println("Hello world!")
+
+    /* Count clicks: */
+    val btn1 = document.getElementById("button-1")
+    val btn1Clicks = document.getElementById("button-1-clicks")
+    btn1.addEventListener("click", { (e0: dom.Event) =>
+      val e = e0.asInstanceOf[dom.MouseEvent]
+      println(s"Click on button '${btn1.id}'. Event: '${e.`type`}'.")
+      val cnt = Integer.parseInt(btn1Clicks.textContent)
+      btn1Clicks.textContent = String.valueOf(cnt + 1)
+    }, false)
+
   }
 }
